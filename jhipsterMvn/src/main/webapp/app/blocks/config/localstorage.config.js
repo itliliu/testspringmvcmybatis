ea052a73117b0fe59cfe5bb2f3ded2b8025fbd07
liliu <liliu@hengtiansoft.com>
2017-06-16 18:34:44 +0800

    init

---
+++ jhipsterMvn/src/main/webapp/app/blocks/config/localstorage.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('testjhipsterMvnApp')
+        .config(localStorageConfig);
+
+    localStorageConfig.$inject = ['$localStorageProvider', '$sessionStorageProvider'];
+
+    function localStorageConfig($localStorageProvider, $sessionStorageProvider) {
+        $localStorageProvider.setKeyPrefix('jhi-');
+        $sessionStorageProvider.setKeyPrefix('jhi-');
+    }
+})();
